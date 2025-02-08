401031ebc55aa59fa46bc6cc28ea2a2ccf022de4
amitb07 <amitbule7@gmail.com>
2025-02-08 20:21:29 +0530

    Page Faults in LRU

---
+++ GFG/Page_Faults_in_LRU.java
@@ -0,0 +1,34 @@
+// Page Faults in LRU
+// https://www.geeksforgeeks.org/problems/page-faults-in-lru5603/1
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+// TimeComplexity O(n)
+class Solution{
+    static int pageFaults(int n, int c, int pages[]){
+        // code here
+        int pageFault=0;
+        List<Integer> q = new ArrayList<>();
+        for(int i: pages) {
+            int ind = q.indexOf(i);
+            if(ind == -1) {
+                 pageFault++;
+                if(q.size() < c)
+                    q.add(i);
+                else {
+                    q.remove(0);
+                    q.add(i);
+                }
+            }
+            else {
+              q.remove(ind);
+              q.add(i);   
+            }
+        }
+        return pageFault;
+    }
+}
